4b7b853c3ffeb45100b4c90b9ee52528c0335b2f
Raven Laing <laingraven.java@gmail.com>
2025-08-10 00:31:36 +0700

    Update RecentColorPaletteData

--- src/main/java/raven/color/component/utils/RecentColorPaletteData.java
+++ src/main/java/raven/color/component/utils/RecentColorPaletteData.java
@@ -1,33 +1,26 @@
 package raven.color.component.utils;
 
 import java.awt.*;
-import java.util.List;
 
 public class RecentColorPaletteData implements ColorPaletteData {
-
-    private List<Color> listColors;
 
     public RecentColorPaletteData() {
         init();
     }
 
     private void init() {
-
     }
 
     @Override
     public void add(Color color) {
-
     }
 
     @Override
     public void remove(Color color) {
-
     }
 
     @Override
     public void clear() {
-
     }
 
     @Override
